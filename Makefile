663f4a3be2ddd8dfad425be8af7a9f58e2c2e0d7
nikbyar <nikbyar@gmail.com>
2021-05-17 23:17:31 +0300

    add brain_progression

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ brain-gcd:
 brain-even:
 	@poetry run brain-even
 
+brain-progr:
+	@poetry run brain-progression
+
 build:
 	poetry build
 
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from brain_games.engine import engine
+from brain_games.games.progression import progression
+
+
+def main():
+    engine()
+    progression()
+
+
+if __name__ == '__main__':
+    main()
