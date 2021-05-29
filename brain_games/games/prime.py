7cd17fc823a0655c24df2bfeacac845d38a0fea3
nikbyar <81713880+nikbyar@users.noreply.github.com>
2021-05-29 15:23:03 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,7 +9,7 @@ def is_prime(number):
     if number < 2:
         return False
     for divisor in range(2, number):
-        if (number % divisor == 0):
+        if number % divisor == 0:
             return False
     return True
 
