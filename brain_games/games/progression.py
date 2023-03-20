55fe9ffd3f478f6dc8b9879ac1bbce4f13101834
ivan <nikbyar@gmail.com>
2023-03-21 00:33:16 +0400

    refactor. separating functions in progression module

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,23 +7,30 @@ WELCOME = 'What number is missing in the progression?'
 TERMS = 10
 
 
+def generate_progression(first_term, common_diff, n):
+    progression = [first_term]
+    for term in range(n - 1):
+        progression.append(progression[term] + common_diff)
+    return progression
+
+
+def progression_to_str(progression, gap_index):
+    gaped_progression = progression[:]
+    gaped_progression[gap_index] = '..'
+    gaped_progression = " ".join([str(i) for i in gaped_progression])
+    return gaped_progression
+
+
 def generate_question_answer():
     gaped_progressions_list = []
     answers_list = []
     for i in range(ATTEMPTS):
-        progression_start = random.randint(FIRST_RANDOM, LAST_RANDOM)
-        diff = random.randint(FIRST_RANDOM, LAST_RANDOM)
+        first_term = random.randint(FIRST_RANDOM, LAST_RANDOM)
+        common_diff = random.randint(FIRST_RANDOM, LAST_RANDOM)
         gap_index = random.randint(FIRST_RANDOM, LAST_RANDOM)
-
-        full_progression = [progression_start]
-        for term in range(TERMS - 1):
-            full_progression.append(full_progression[term] + diff)
-
-        answer = str(full_progression[gap_index])
-
-        gaped_progression = full_progression[:]
-        gaped_progression[gap_index] = '..'
-        gaped_progression = " ".join([str(i) for i in gaped_progression])
+        progression = generate_progression(first_term, common_diff, TERMS)
+        gaped_progression = progression_to_str(progression, gap_index)
+        answer = str(progression[gap_index])
 
         gaped_progressions_list.append(gaped_progression)
         answers_list.append(answer)
