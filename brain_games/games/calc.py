406525e9d566de75e6a93b7dd559e61ba25c0258
ivan <nikbyar@gmail.com>
2023-03-24 17:33:15 +0400

    second mentor's check. correct complexity of generate_question_answer in calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,12 +9,10 @@ NUMBER_OF_RANDOM_NUMBERS_IN_EXPRESSION = 2
 
 
 def generate_question_answer():
-    random_ints = []
-    for i in range(ROUNDS):
-        question = []
-        for j in range(NUMBER_OF_RANDOM_NUMBERS_IN_EXPRESSION):
-            question.append(random.randint(LOWER_BOUND, UPPER_BOUND))
-        random_ints.append(question)
+    random_ints = [[random.randint(LOWER_BOUND, UPPER_BOUND)
+                    for i in range(0, 2)] for j in range(0, ROUNDS)]
+    # создание списка через генератор списков
+
 
     random_signs = random.choices(SIGNS, k=ROUNDS)
     result_list = []
