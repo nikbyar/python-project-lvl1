6f370191ab7144a69fbbae78edcbb95b66a728be
ivan <nikbyar@gmail.com>
2023-03-24 04:21:20 +0400

    second mentor's check. refactor creating random_ints through for loops.

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,28 +1,37 @@
 import random
 from brain_games.engine import ROUNDS
 
-
 LOWER_BOUND = 1
 UPPER_BOUND = 10
 WELCOME = 'What is the result of the expression?'
 SIGNS = ['+', '-', '*']
+NUMBER_OF_RANDOM_NUMBERS_IN_EXPRESSION = 2
 
 
 def generate_question_answer():
-    random_ints = [[random.randint(LOWER_BOUND, UPPER_BOUND)
-                    for i in range(0, 2)] for j in range(0, ROUNDS)]
-    random_signs = random.choices(SIGNS, k=ROUNDS)
-    result = []
+    # random_ints = [[random.randint(LOWER_BOUND, UPPER_BOUND)
+    #                 for i in range(0, 2)] for j in range(0, ROUNDS)]
+    # создание списка через генератор списков
 
-    for i in range(0, ROUNDS):
+    random_ints = []
+    for i in range(ROUNDS):
+        question = []
+        for j in range(NUMBER_OF_RANDOM_NUMBERS_IN_EXPRESSION):
+            question.append(random.randint(LOWER_BOUND, UPPER_BOUND))
+        random_ints.append(question)
+
+    random_signs = random.choices(SIGNS, k=ROUNDS)
+    result_list = []
+
+    for i in range(ROUNDS):
         if random_signs[i] == '+':
-            a = random_ints[i][0] + random_ints[i][1]
+            result = random_ints[i][0] + random_ints[i][1]
         elif random_signs[i] == '-':
-            a = random_ints[i][0] - random_ints[i][1]
+            result = random_ints[i][0] - random_ints[i][1]
         elif random_signs[i] == '*':
-            a = random_ints[i][0] * random_ints[i][1]
-        result.append(str(a))
+            result = random_ints[i][0] * random_ints[i][1]
+        result_list.append(str(result))
 
     question = [f'{(random_ints[i][0])} {random_signs[i]} '
-                f'{(random_ints[i][1])}' for i in range(0, ROUNDS)]
-    return question, result
+                f'{(random_ints[i][1])}' for i in range(ROUNDS)]
+    return question, result_list
