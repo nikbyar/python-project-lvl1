94b9e7095a10405bea835a9cbaa7b3cf2c7a2568
ivan <nikbyar@gmail.com>
2023-03-24 17:04:47 +0400

    second mentor's check. refactor gcd, using Euclides algorithm and changed creating random_ints.

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,21 +8,30 @@ WELCOME = 'Find the greatest common divisor of given numbers.'
 
 
 def get_gcd(pair):
-    gcd = '1'
-    for j in range(min(pair[0], pair[1]), 1, -1):
-        if pair[0] % j == 0:
-            if pair[1] % j == 0:
-                gcd = str(j)
-                return gcd
-    return gcd
+    a, b = pair
+    while a and b > 0:
+        if a >= b:
+            a = a % b
+        elif a < b:
+            b = b % a
+    return max(a, b)
 
 
 def generate_question_answer():
-    random_ints = [[random.randint(LOWER_BOUND, UPPER_BOUND)
-                    for i in range(0, 2)] for j in range(0, ROUNDS)]
+    # random_ints = [[random.randint(LOWER_BOUND, UPPER_BOUND)
+    #                 for i in range(0, 2)] for j in range(0, ROUNDS)]
+    # создание списка через генератор списков
+
+    random_ints = []
+    for i in range(ROUNDS):
+        question = []
+        for j in range(2):
+            question.append(random.randint(LOWER_BOUND, UPPER_BOUND))
+        random_ints.append(question)
+
     result = []
     for pair in random_ints:
-        result.append(get_gcd(pair))
+        result.append(str(get_gcd(pair)))
 
     question = [' '.join((map(str, j))) for j in random_ints]
     return question, result
